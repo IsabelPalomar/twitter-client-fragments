8f1dc4528957833ca77ed049a910f3b8020ed287
Isabel Palomar <isabel.palomarb@gmail.com>
2016-08-06 00:02:39 -0400

    While composing a tweet, user can see a character counter with characters remaining for tweet out of 140

--- app/src/main/java/io/androidblog/apps/mysimpletweets/fragments/ComposeTweetDialogFragment.java
+++ app/src/main/java/io/androidblog/apps/mysimpletweets/fragments/ComposeTweetDialogFragment.java
@@ -3,17 +3,29 @@ package io.androidblog.apps.mysimpletweets.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import io.androidblog.apps.mysimpletweets.R;
 
 public class ComposeTweetDialogFragment extends DialogFragment {
 
-    private EditText mEditText;
+    @BindView(R.id.etTweet)
+    EditText etTweet;
+    @BindView(R.id.btnClose)
+    Button btnClose;
+    @BindView(R.id.tvChars)
+    TextView tvChars;
 
     public ComposeTweetDialogFragment() {
     }
@@ -21,18 +33,44 @@ public class ComposeTweetDialogFragment extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_tweet, container);
+        View view = inflater.inflate(R.layout.fragment_tweet, container);
+        ButterKnife.bind(this, view);
+        return view;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         // Get field from view
-        mEditText = (EditText) view.findViewById(R.id.etTweet);
+
+        final TextWatcher mTextEditorWatcher = new TextWatcher() {
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                tvChars.setText(String.valueOf(140 - s.length()));
+            }
+
+            public void afterTextChanged(Editable s) {
+            }
+        };
+
+        etTweet.addTextChangedListener(mTextEditorWatcher);
+
         // Show soft keyboard automatically and request focus to field
-        mEditText.requestFocus();
         getDialog().getWindow().setSoftInputMode(
-                WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        etTweet.requestFocus();
+
+    }
+
+    @Override
+    public void onActivityCreated(Bundle arg0) {
+        super.onActivityCreated(arg0);
+        getDialog().getWindow()
+                .getAttributes().windowAnimations = R.style.DialogAnimation;
     }
 
     @Override
@@ -42,10 +80,15 @@ public class ComposeTweetDialogFragment extends DialogFragment {
         // Assign window properties to fill the parent
         params.width = WindowManager.LayoutParams.MATCH_PARENT;
         params.height = WindowManager.LayoutParams.MATCH_PARENT;
-        getDialog().getWindow().setAttributes((android.view.WindowManager.LayoutParams) params);
+        getDialog().getWindow().setAttributes((WindowManager.LayoutParams) params);
         // Call super onResume after sizing
         super.onResume();
     }
 
+
+    @OnClick(R.id.btnClose)
+    public void closeDialog() {
+        dismiss();
+    }
 
 }
